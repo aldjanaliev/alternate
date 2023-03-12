606ffd8fdc39603b31b54558e0acdf25b943041d
aldjanaliev <aldjanaliev@gmail.com>
2023-03-12 16:49:19 +0600

    desktop

---
+++ js/core.js
@@ -0,0 +1,153 @@
+$(document).ready(function() {
+	// ====== inputmask ======
+	$('[type="tel"]').inputmask('+7 (999) 999-99-99');
+
+	$('.main_slider').each(function(i, e){
+    let slideTab = $(e).closest('.slider_wrap').find('.tab_slider')
+    $(e).slick({
+      dots: false,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    	//autoplay: 300,
+      asNavFor: slideTab
+    })
+	})
+	$('.tab_slider').each(function(i, e){
+    let slideTab = $(e).closest('.slider_wrap').find('.main_slider')
+    let numTabSlider = 4
+    if($(e).closest('.works_card').length){
+      numTabSlider = 3
+    }
+    $(e).slick({
+      dots: false,
+      arrows: false,
+      infinite: false,
+      slidesToShow: numTabSlider,
+      slidesToScroll: 1,
+    //   autoplay: 300,
+      asNavFor: slideTab,
+      focusOnSelect: true
+    })
+	})
+
+  $('.sale_slider').slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  //   autoplay: 300,
+  })
+
+	if(document.querySelector('.to-show_btn')){
+		let historyBtn = document.querySelectorAll('.to-show_btn')
+    historyBtn.forEach(el => {
+      el.addEventListener('click', function(){
+        let blockHide = $(this).closest('section').find('.block__hide')
+        if(this.textContent === 'Показать ещё'){
+          blockHide.slideDown(300)
+          blockHide.find('.slider_wrap > div').slick('refresh')
+          this.textContent = 'Скрыть'
+        } else{
+          blockHide.slideUp(300)
+          this.textContent = 'Показать ещё'
+        }
+      })
+    })
+	}
+
+  if(document.querySelector('.widget_close')){
+    const widgetClose = document.querySelector('.widget_close')
+    widgetClose.addEventListener('click', function(){
+      const widget = document.querySelector('.widget')
+      widget.style.opacity = 0
+      setTimeout(() => {
+        widget.style.display = "none"
+      },300)
+    })
+  }
+
+  $('.scroll').on('click', function(){
+    let href = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+  })
+
+  if(document.querySelector('.sale_card-btn')){
+    const saleBtn = document.querySelectorAll('.sale_card-btn')
+    saleBtn.forEach(btn => {
+      btn.addEventListener('click', function(){
+        if(this.closest('.sale_slide-in').className != 'active'){
+          if(document.querySelector('.sale_slide-in.active')){
+            document.querySelector('.sale_slide-in.active').classList.remove('active')
+          }
+          this.closest('.sale_slide-in').classList.add('active')
+        }
+      })
+    })
+    const saleClose = document.querySelectorAll('.sale_form-close')
+    saleClose.forEach(close => {
+      close.addEventListener('click', function(){
+        this.closest('.sale_slide-in').classList.remove('active')
+      })
+    })
+  }
+
+  // ====== start  дата в акции ======
+  var options = {
+    month: 'long',
+    day: 'numeric',
+  };
+  let date = new Date();
+  date.setDate(date.getDate());
+  let m = date.getMonth()+1;
+  let date2 = new Date(2023, m, 0)
+
+  date = date.toLocaleString("ru", options)
+  date = date.split(' ')[1]
+
+  $('.js-day').html(date2.getDate() + " " + date );
+
+  switch(m){
+    case 1:
+      $('.js-month').html("январе")
+      break;
+    case 2:
+      $('.js-month').html("феврале")
+      break;
+    case 3:
+      $('.js-month').html("марте")
+      break
+    case 4:
+      $('.js-month').html("апреле")
+      break;
+    case 5:
+      $('.js-month').html("мае")
+      break;
+    case 6:
+      $('.js-month').html("июне")
+      break;
+    case 7:
+      $('.js-month').html("июле")
+      break
+    case 8:
+      $('.js-month').html("августе")
+      break;
+    case 9:
+      $('.js-month').html("сентябре")
+      break;
+    case 10:
+      $('.js-month').html("октябре")
+      break;
+    case 11:
+      $('.js-month').html("ноябре")
+      break
+    case 12:
+      $('.js-month').html("декабре")
+      break;
+  }
+
+})
---
+++ mail.php
@@ -0,0 +1,101 @@
+<?php 
+	
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'PHPMailer/src/Exception.php';
+	require 'PHPMailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer;
+	$mail->CharSet = 'UTF-8';
+	$mail->IsHTML(true);
+
+	// от кого письмо
+	$mail->setFrom('info@' . $_SERVER['HTTP_HOST'], 'Элайнеры - promo');
+	// кому отправить
+	$admin_email  = ['quiz24-job@yandex.ru'];
+	// $admin_email  = ['quiz24-job@yandex.ru', 'info@terminator.ru'];
+	foreach ( $admin_email as $key => $value ) {
+		$mail->addAddress($value);
+	}
+	$form_subject = 'Заявка с сайта promo.eurokappa.moscow';
+	$mail->Subject = $form_subject;
+
+	$c = true;
+	$message = '';
+	// $message2 = '';
+	foreach ( $_POST as $key => $value ) {
+		if ( $value != ""  && $key != "admin_email" && $key != "form_subject" ) {
+			if (is_array($value)) {
+				$val_text = '';
+				foreach ($value as $val) {
+					if ($val && $val != '') {
+						$val_text .= ($val_text==''?'':', ').$val;
+					}
+				}
+				$value = $val_text;
+			}
+			// $message2 .= "{$key}: {$value} \n";
+
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+			<td style='padding: 10px; width: auto;'><b>$key:</b></td>
+			<td style='padding: 10px;width: 100%;'>$value</td>
+			</tr>
+			";
+
+		}
+	}
+
+	$message = "<table style='width: 50%;'>$message</table>";
+
+	// if(!empty($_FILES['file']['tmp_name'])){
+	// 	$filePath = __DIR__ . "/upload/" . $_FILES['file']['name'];
+	// 	if(copy($_FILES['file']['tmp_name'], $filePath)){
+	// 		$fileAttach = $filePath;
+	// 		$message .= '<p><b>Файл в приложении:</b></p>';
+	// 		$mail->addAttachment($fileAttach);
+	// 	}
+	// }
+	
+	$body = $message;
+	// $mail->isHTML(true);  это если прям верстка
+	$mail->msgHTML($body);
+
+	$mail->send();
+
+	$response = ['message' => $message];
+	echo json_encode($response);
+
+	//require_once ('query/index.php');
+
+	//Добавляем лид  
+    $srm_lead_url = "https://eurokappa.bitrix24.ru/rest/5274/ocqpp8glkn36y0l1/crm.lead.add";
+    $srm_lead_add = http_build_query(array(
+      'fields' => array(
+        "TITLE" => $title,
+        "NAME" => $name,
+        "OPENED" => "Y",
+        "PHONE" => array(
+          array("VALUE" => $phone, "VALUE_TYPE" => "WORK")
+        ),
+      ),
+      'params' => array("REGISTER_SONET_EVENT" => "Y")
+    ));
+
+    $curl_lead_add = curl_init();
+    curl_setopt_array($curl_lead_add, array(
+      CURLOPT_SSL_VERIFYPEER => 0,
+      CURLOPT_POST => 1,
+      CURLOPT_HEADER => 0,
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_URL => $srm_lead_url,
+      CURLOPT_POSTFIELDS => $srm_lead_add,
+    ));
+    $new_lead = curl_exec($curl_lead_add);
+    curl_close($curl_lead_add);
+
+    $new_lead = json_decode($new_lead, 1);
+
+    $leadID = isset($new_lead['result']) ? $new_lead['result'] : 0;
+    echo '$leadID = ' . $leadID;
